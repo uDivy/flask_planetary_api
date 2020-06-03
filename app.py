7b2c79882dd15a44cb1ea78277c8c8cc749351c5
uDivy <upadhyaydivy10@gmail.com>
2020-06-03 16:00:41 +0530

    Course Completed

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_mail import Mail, Message
 
 import os
 
@@ -9,9 +11,15 @@ app = Flask(__name__)
 # to put the file in same folder
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+app.config['JWT_SECRET_KEY'] = "super-secret"
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 
 db = SQLAlchemy(app)  # initialise the db
 ma = Marshmallow(app) # to serialize
+jwt = JWTManager(app) # for authentication
+mail = Mail(app) # for sending mail
 
 
 @app.cli.command('db_create')
@@ -67,44 +75,126 @@ def db_seed():
     print("Database seeded!")
 
 
-@app.route('/')
-def name():
-    return 'Hello World'
-
-
-@app.route('/super_simple')
-def super_simple():
-    return jsonify(message='Hello from the Planetary API.')
-
-
-@app.route('/not_found')
-def not_found():
-    return jsonify(message='That resource was not found'),404
-
-
-@app.route('/parameteres')
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(message="Sorry {0}, you are not old enough.".format(name)), 401
-    else:
-        return jsonify(message="Welcome {0}, you are old enough.".format(name))
-
-
-@app.route('/url_variables/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-    if age < 18:
-        return jsonify(message="Sorry {0}, you are not old enough.".format(name)), 401
-    else:
-        return jsonify(message="Welcome {0}, you are old enough.".format(name))
-
 
 @app.route('/planets',methods=["GET"])
 def planets():
     planets_list = Planet.query.all()
     result = planets_schema.dump(planets_list) # dump to serialise(textual format)
     return jsonify(result)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message="That email already exists."), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+
+    user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(message="User added successfully"), 201
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message="Login Successful", access_token=access_token)
+    else:
+        return jsonify(message="Login Failed!"), 401
+
+
+@app.route('/retrieve_password/<string:email>', methods=['GET'])
+def retrieve_password(email: str):
+    user = User.query.filter_by(email=email).first()
+    if user:
+        msg = Message("your planetary API password is "+ user.password,
+                      sender="admin@planetary-api.com",
+                      recipients=[email]
+                      )
+        mail.send(msg)
+        return jsonify(message="Password sent to "+ email)
+    else:
+        return jsonify(message="That email doesn't exist"), 401
+
+
+@app.route('/planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = planet_schema.dump(planet)
+        return jsonify(result)
+    else:
+        return jsonify(message="That planet does not exist"), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required
+def add_planet():
+    planet_name = request.form['planet_name']
+
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message='Planet already exists!'), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float( request.form['distance'])
+        new_planet = Planet(
+            planet_name=planet_name,
+            planet_type=planet_type,
+            home_star=home_star,
+            mass=mass,
+            radius=radius,
+            distance=distance
+        )
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message="Planet added successfully!"), 201
+
+
+@app.route('/update_planet', methods=['POST','PUT'])
+@jwt_required
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="Planet " + planet.planet_name + " Updated Successfully"), 202
+    else:
+        return jsonify(message="Planet not exist, create one"), 404
+
+
+@app.route('/remove_planet/<int:planet_id>', methods=['DELETE'])
+@jwt_required
+def remove_planet(planet_id:int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message="Planet Deleted!"), 202
+    else:
+        return jsonify(message="Planet not exists!"), 404
 
 
 # database models
